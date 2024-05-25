71533bcc6bb3e23fc013abe28c27572439a4f491
Joel <52992874+xsrjowi@users.noreply.github.com>
2024-05-25 15:45:37 +0200

    🧑 User CRUD

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const app = express()
 
 app.use(bodyParser.json())
 
+// ? Importación rutas:
+const { getUsers, createUser, editUser, deleteUser } = require("./routes/users/users")
+
+app.get('/users', getUsers)
+app.post('/users', createUser)
+app.put('/users/:id', editUser)
+app.delete('/users/:id', deleteUser)
+
 const port = settings.express.app_port
 console.log(port)
 app.listen(port, () => {
---
+++ routes/users/users.js
@@ -0,0 +1,80 @@
+const mysql = require('../../database/pool')
+
+// ? Get all database users
+const getUsers = async(req, res) => {
+    try {
+        const [rows] = await mysql.query('SELECT * FROM Users')
+        return res.json(rows)
+    } catch (error) {
+        res.status(500).json({
+            error: error.message,
+        })
+    }
+}
+
+// ? Create new user
+// TODO: Guard - User has permissions
+const createUser = async(req, res) => {
+    const { name, birth_date, email, password, profile_image } = req.body
+    try {
+        // TODO: Validator - Ensure that req.body data is válid
+
+        const [result] = await mysql.query(
+            'INSERT INTO Users (name, birth_date, email, password, profile_image) VALUES (?, ?, ?, ?, ?)',
+            [name, birth_date, email, password, profile_image]
+        )
+
+        return res.json({ 
+            id: result.insertId,
+            name: result.name, 
+            status: 200,
+        })
+    } catch (error) {
+        return res.status(500).json({
+            error: error.message
+        })
+    }
+}
+
+// ? Edit an exisitng user
+// TODO: Guard - User has permissions
+const editUser = async(req, res) => {
+    const { id } = req.params
+    const { name, birth_date, email, password, profile_image } = req.body
+
+    try {
+        const [result] = await mysql.query(
+            'UPDATE Users SET name = ?, birth_date = ?, email = ?, password = ?, profile_image = ? WHERE id = ?',
+            [name, birth_date, email, password, profile_image, id]
+        )
+
+        return res.json(result, { status: 200 })
+    } catch (error) {
+        return res.status(500).json({
+            error: error.message
+        })
+    }
+}
+
+// ? Delete an existing user
+// TODO: Guard - User has permissions
+const deleteUser = async (req, res) => {
+    const { id } = req.params
+    
+    try {
+        const [result] = await mysql.query(
+            'DELETE FROM Users WHERE id = ?',
+            [id]
+        )
+
+        return res.json({ 
+            status: 200
+        })
+    } catch (error) {
+        return res.status(500).json({
+            error: error.message
+        })
+    }
+}
+
+module.exports = { getUsers, createUser }
